576a3758b9a18b62d764cb581becd98cb066d0c6
Pablo Francisco Mendoza Vargas <pfmv@Pablos-MacBook-Pro.local>
2020-04-10 17:41:06 -0500

    Updates

---
+++ precache-manifest.04ca6fefa6a696284a17d4d3c50aa71e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f747aaf5921f22d89200533b9f233135",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "1eeb21e81b23a21a2048",
+    "url": "/crwn-clothing/static/css/main.92c754c2.chunk.css"
+  },
+  {
+    "revision": "08fed3c85594328c2c2f",
+    "url": "/crwn-clothing/static/js/2.5f9fd417.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crwn-clothing/static/js/2.5f9fd417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eeb21e81b23a21a2048",
+    "url": "/crwn-clothing/static/js/main.1f534a3a.chunk.js"
+  },
+  {
+    "revision": "47f16e26d356a7dc7bf9",
+    "url": "/crwn-clothing/static/js/runtime-main.56f076f1.js"
+  }
+]);
