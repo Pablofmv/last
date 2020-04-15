52ba9d138806d040ae6bdb504d0c7951be7b0825
Pablo Francisco Mendoza Vargas <pfmv@Pablos-MacBook-Pro.local>
2020-04-14 22:06:30 -0500

    deploy web with header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/hompage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
       <Route exact path='/crwn-clothing' component={HomePage} />
       <Route path='/crwn-clothing/shop' component={ShopPage} />   
