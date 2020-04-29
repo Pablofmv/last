81b3c96539892f8072ee146e47029d2993db96a2
Pablo Francisco Mendoza Vargas <pfmv@Pablos-MacBook-Pro.local>
2020-04-29 12:20:27 -0500

    updated shop data with new data

---
+++ src/redux/cart/cart.reducer.js
@@ -0,0 +1,28 @@
+import CartActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils';
+
+
+
+const INITIAL_STATES = {
+    hidden: true,
+    cartItems: []
+};
+
+const cartReducer = (state=INITIAL_STATES,action)=>{
+    switch(action.type){
+        case CartActionTypes.TOGGLE_CART_HIDDEN:
+            return {
+                ...state,
+                hidden: !state.hidden
+            };
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload)
+            };
+        default:
+                return state;
+    }
+};
+
+export default cartReducer;
