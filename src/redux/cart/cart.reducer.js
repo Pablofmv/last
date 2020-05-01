6b70076692267b01cc16051078b11ab307b52138
Pablo Francisco Mendoza Vargas <pfmv@Pablos-MacBook-Pro.local>
2020-05-01 18:55:14 -0500

    added addItem and removeItem

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,6 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
+import cartItem from '../../components/cart-item/cart-item.component';
 
 
 
@@ -19,6 +20,18 @@ const cartReducer = (state=INITIAL_STATES,action)=>{
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            };
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            };
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ... state,
+                cartItems:  state.cartItems.filter(
+                    cartItem=>cartItem.id !== action.payload.id
+                )
             };
         default:
                 return state;
