6a986446f85c205ec550f06e865e2973a4a1c083
Pablo Francisco Mendoza Vargas <pfmv@Pablos-MacBook-Pro.local>
2020-05-05 12:10:04 -0500

    integrate stripe

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_lTJBt93fC4QxQkMvLsTABDIk';
+
+     const onToken = token => {
+        console.log(token);
+        alert ('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout 
+        label = 'Pay Now'
+        name = 'CRWN Clothing LTD'
+        billingAddress
+        shippingAddress
+        image = 'https://svgshare.com/i/CUz.svg'
+        description = {`Your total is $${price}`}
+        amount = {priceForStripe}
+        panelLabel = 'Pay Now'
+        token={onToken}
+        stripeKey={publishableKey}
+        />
+    );
+
+};
+
+export default StripeCheckoutButton;
